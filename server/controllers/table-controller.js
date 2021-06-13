933f0b81a24d9943a9756e651b25f13859a62c7b
ovalle15 <74880762+ovalle15@users.noreply.github.com>
2021-06-13 17:52:10 -0400

    removed xml response

--- server/controllers/table-controller.js
+++ server/controllers/table-controller.js
@@ -28,7 +28,7 @@ getItems =  async(req, res) => {
             .status(200)
             .json({
                 success: true,
-                item: items,
+                items: items,
             });
     }).catch(err => {
         console.error(`caught error in 'getItemsById': ${err}`);
