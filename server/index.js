44600089def58c5ece57b9b19eea9eb7acb7d698
ovalle15 <74880762+ovalle15@users.noreply.github.com>
2021-07-09 12:02:12 -0400

    test

--- server/index.js
+++ server/index.js
@@ -20,21 +20,21 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 if (process.env.NODE_ENV === 'production'){
     console.log("env ======>", process.env.NODE_ENV)
 
-    app.use((req, res, next) => {
+    // app.use((req, res, next) => {
 
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'https://management-mern-app.herokuapp.com');
+    //     // Website you wish to allow to connect
+    //     res.setHeader('Access-Control-Allow-Origin', 'https://management-mern-app.herokuapp.com');
 
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //     // Request methods you wish to allow
+    //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //     // Request headers you wish to allow
+    //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-        // Pass to next layer of middleware
-        next();
+    //     // Pass to next layer of middleware
+    //     next();
 
-      });
+    //   });
 
     app.use('/api', routerTable);
 
